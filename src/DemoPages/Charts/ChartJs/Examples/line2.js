d6bea7d16c18e89cd8b5b73ac9dc4b6ae737e650
efranco98 <efranco98@hotmail.com>
2020-09-26 21:28:20 -0600

    mia de mi

--- src/DemoPages/Charts/ChartJs/Examples/line2.js
+++ src/DemoPages/Charts/ChartJs/Examples/line2.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {Line} from 'react-chartjs-2';
 
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','agosto'],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'Mia de mi',
       fill: true,
       lineTension: 0.1,
       backgroundColor: 'rgba(75,192,192,0.4)',
@@ -23,7 +23,7 @@ const data = {
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: [65, 59, 80, 81, 56, 55, 40,25]
     }
   ]
 };
