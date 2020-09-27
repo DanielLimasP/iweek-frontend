5f2239ed8518088af56b050aa895d19c20d5d0a2
Daniel Limas <amedlimas97@gmail.com>
2020-09-26 19:59:38 -0600

    Removed some little things

--- src/Layout/AppHeader/Components/UserBox.js
+++ src/Layout/AppHeader/Components/UserBox.js
@@ -31,7 +31,7 @@ class UserBox extends React.Component {
 
     }
 
-    notify2 = () => this.toastId = toast("You don't have any new items in your calendar for today! Go out and play!", {
+    notify2 = () => this.toastId = toast("Para empezar a utilizar la app, selecciona un indicador y realiza una busqueda", {
         transition: Bounce,
         closeButton: true,
         autoClose: 5000,
@@ -48,55 +48,13 @@ class UserBox extends React.Component {
                     <div className="widget-content p-0">
                         <div className="widget-content-wrapper">
                             <div className="widget-content-left">
-                                <UncontrolledButtonDropdown>
-                                    <DropdownToggle color="link" className="p-0">
-                                        <img width={42} className="rounded-circle" src={avatar1} alt=""/>
-                                        <FontAwesomeIcon className="ml-2 opacity-8" icon={faAngleDown}/>
-                                    </DropdownToggle>
-                                    <DropdownMenu right className="rm-pointers dropdown-menu-lg">
-                                        <Nav vertical>
-                                            <NavItem className="nav-item-header">
-                                                Activity
-                                            </NavItem>
-                                            <NavItem>
-                                                <NavLink href="javascript:void(0);">
-                                                    Chat
-                                                    <div className="ml-auto badge badge-pill badge-info">8</div>
-                                                </NavLink>
-                                            </NavItem>
-                                            <NavItem>
-                                                <NavLink href="javascript:void(0);">Recover Password</NavLink>
-                                            </NavItem>
-                                            <NavItem className="nav-item-header">
-                                                My Account
-                                            </NavItem>
-                                            <NavItem>
-                                                <NavLink href="javascript:void(0);">
-                                                    Settings
-                                                    <div className="ml-auto badge badge-success">New</div>
-                                                </NavLink>
-                                            </NavItem>
-                                            <NavItem>
-                                                <NavLink href="javascript:void(0);">
-                                                    Messages
-                                                    <div className="ml-auto badge badge-warning">512</div>
-                                                </NavLink>
-                                            </NavItem>
-                                            <NavItem>
-                                                <NavLink href="javascript:void(0);">
-                                                    Logs
-                                                </NavLink>
-                                            </NavItem>
-                                        </Nav>
-                                    </DropdownMenu>
-                                </UncontrolledButtonDropdown>
                             </div>
                             <div className="widget-content-left  ml-3 header-user-info">
                                 <div className="widget-heading">
-                                    Alina Mclourd
+                                    Secretaria de turismo
                                 </div>
                                 <div className="widget-subheading">
-                                    VP People Manager
+                                    Gobierno del Estado de Chihuahua
                                 </div>
                             </div>
 
